9248f3fc7cf339e89fa8429201a4e07b0e1ebb7c
Timoideas <Timoideas@hotmail.com>
2021-05-28 08:59:31 -0500

    Comunicados component

---
+++ components/home/Comunicados.js
@@ -0,0 +1,9 @@
+import style from './Comunicados.module.css';
+function Comunicados() {
+	return (
+		<div className={style.Comunicados}>
+			<h2>Comunicados</h2>
+		</div>
+	);
+}
+export default Comunicados;
